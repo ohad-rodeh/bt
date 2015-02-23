d7d78fcb4edb9917e220b34f311db5abd6fd448b
Ohad Rodeh <orodeh@dnanexus.com>
2015-02-23 06:11:26 -0800

    Adding 2015 to the coypright notice

--- src/tar/Makefile
+++ src/tar/Makefile
@@ -1,6 +1,6 @@
 #*************************************************************#
 #
-# Copyright (c) 2014, Ohad Rodeh, IBM Research
+# Copyright (c) 2014-2015, Ohad Rodeh, IBM Research
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
--- src/tools/convert_Makefile.sh
+++ src/tools/convert_Makefile.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -f 
 
-for i in `find . -name "files.mk"`
+#for i in `find . -name "files.mk"`
+for i in `find . -name "Makefile"`
 do 
   echo "converting " $i
 
-  ~/store/OSD/SA/src/tools/convert_Makefile.awk $i > M
+  #~/store/OSD/SA/src/tools/convert_Makefile.awk $i > M
+  #mv M $i
+  sed -f /home/orodeh/U/git/orodeh/bt/src/tools/convert_files.sed $i > M
   mv M $i
 done
